38d1a198b3d5701a637353f0d23cd53d6cbbcda9
Michal Cichon <michal.cichon@aol.com>
2013-09-01 01:40:09 +0200

    10. All katakana symbols update

--- src/com/cichon/writekananow/KatakanaFactory.java
+++ src/com/cichon/writekananow/KatakanaFactory.java
@@ -18,6 +18,59 @@ public class KatakanaFactory {
 		dictionary.add("e");
 		dictionary.add("o");
 		
+		dictionary.add("ka");
+		dictionary.add("ki");
+		dictionary.add("ku");
+		dictionary.add("ke");
+		dictionary.add("ko");
+		
+		dictionary.add("sa");
+		dictionary.add("shi");
+		dictionary.add("su");
+		dictionary.add("se");
+		dictionary.add("so");
+		
+		dictionary.add("ta");
+		dictionary.add("chi");
+		dictionary.add("tsu");
+		dictionary.add("te");
+		dictionary.add("to");
+		
+		dictionary.add("na");
+		dictionary.add("ni");
+		dictionary.add("nu");
+		dictionary.add("ne");
+		dictionary.add("no");
+		
+		dictionary.add("ha");
+		dictionary.add("hi");
+		dictionary.add("fu");
+		dictionary.add("he");
+		dictionary.add("ho");
+		
+		dictionary.add("ma");
+		dictionary.add("mi");
+		dictionary.add("mu");
+		dictionary.add("me");
+		dictionary.add("mo");
+		
+		dictionary.add("ya");
+		dictionary.add("yu");
+		dictionary.add("yo");
+		
+		dictionary.add("ra");
+		dictionary.add("ri");
+		dictionary.add("ru");
+		dictionary.add("re");
+		dictionary.add("ro");
+		
+		dictionary.add("wa");
+		dictionary.add("wi");
+		dictionary.add("we");
+		dictionary.add("wo");
+		
+		dictionary.add("n");
+		
 		randomGenerator = new Random();
 		
 	}
