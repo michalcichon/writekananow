27c94370ce02d1fcd915668027f91445978e9f50
Michal Cichon <michal.cichon@aol.com>
2013-08-31 18:18:15 +0200

    8. Settings

--- src/com/cichon/writekananow/MainActivity.java
+++ src/com/cichon/writekananow/MainActivity.java
@@ -1,8 +1,13 @@
 package com.cichon.writekananow;
 
+import java.util.Locale;
+
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,22 +18,28 @@ public class MainActivity extends Activity implements OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadPreferences();
         setContentView(R.layout.activity_main);
         
         //bind menu listeners
         findViewById(R.id.button_learn).setOnClickListener(this);
-        findViewById(R.id.button_quiz).setOnClickListener(this);
-        
+        findViewById(R.id.button_quiz).setOnClickListener(this);     
     }
-
-
+    
+    protected void onRestart(){
+    	super.onRestart();
+    	loadPreferences();
+		Intent intent = getIntent();
+		finish();
+		startActivity(intent);
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
 
 	@Override
 	public void onClick(View v) {
@@ -55,5 +66,20 @@ public class MainActivity extends Activity implements OnClickListener {
     	}
     	return false;
     }
+	
+	private void loadPreferences() {
+		
+		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+		String type_lang  = pref.getString("list_language", "en");
+		
+        Locale locale= new Locale(type_lang);
+        Locale.setDefault(locale);
+        Configuration conf = new Configuration();
+        conf.locale = locale;
+        
+        getApplicationContext().getResources().updateConfiguration(conf, 
+        		getApplicationContext().getResources().getDisplayMetrics());
+		
+	}
     
 }
